01cc2b11ea5ff5a750cec75455d5cfb62cde9294
Abuzar <abuzar0561446@gmail.com>
2024-04-28 18:45:42 +0530

    Implement About me section with a appealing lamp effect ui

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import { StickyScroll } from "./ui/sticky-scroll-reveal";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { AboutMeLampEffect } from "./ui/lamp-effect";
+import Loader from "./Loader";
+
+export function AboutMe() {
+  const [activeCard, setActiveCard] = React.useState(0);
+  const backgroundColors = [
+    "var(--slate-900)",
+    "var(--black)",
+    "var(--neutral-900)",
+  ];
+  return (
+    <motion.div
+      animate={{
+        backgroundColor: backgroundColors[activeCard % backgroundColors.length],
+      }}
+      className=" flex flex-col justify-center relative space-x-10 rounded-md p-10"
+    >
+      <div>
+        <h1 className="mt-20 md:mt-0 text-2xl md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 text-center mb-8 ">
+          About me
+        </h1>
+        <AboutMeLampEffect/>
+   
+      </div>
+    
+    </motion.div>
+  );
+}

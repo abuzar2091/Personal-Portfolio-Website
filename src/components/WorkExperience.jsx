4a2beb574679210cc6d0292893dcc6a613965988
Abuzar <abuzar0561446@gmail.com>
2024-04-30 05:01:08 +0530

    Add work experience section to showcase professional history

---
+++ src/components/WorkExperience.jsx
@@ -0,0 +1,33 @@
+"use client";
+import { HoverEffect } from "./ui/card-hover-effect";
+import workExperience from "../data/work_experience.json";
+
+function WorkExperience() {
+  
+  const workExperiences = workExperience.workExperiences;
+  return (
+    <div className="p-12 bg-gray-900">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="text-center">
+          <h1 className=" text-2xl md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 text-center mb-8">
+            WorkExperience
+          </h1>
+        </div>
+
+        <div className="mt-10">
+          <HoverEffect
+            items={workExperiences.map((company) => ({
+              name: company.name,
+              role: company.role,
+              description: company.description,
+              url: company.companyLogo,
+              link: "/",
+            }))}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WorkExperience;
---
+++ src/components/ui/card-hover-effect.jsx
@@ -0,0 +1,102 @@
+import { cn } from "@/utils/cn";
+import { AnimatePresence, motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+export const HoverEffect = ({
+  items,
+  className,
+}) => {
+  let [hoveredIndex, setHoveredIndex] = useState(null);
+
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  py-10",
+        className
+      )}
+      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',height:'500px' }}
+    >
+      {items.map((item, idx) => (
+        <Link
+          href={item?.link}
+          key={item?.link}
+          className="relative group  block p-2 h-full w-full"
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence>
+            {hoveredIndex === idx && (
+              <motion.span
+                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
+                layoutId="hoverBackground"
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.15 },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.15, delay: 0.2 },
+                }}
+              />
+            )}
+          </AnimatePresence>
+          <Card className={"flex"}>
+            <Image src={item.url} height={80} width={80}
+            className="rounded-full"/>
+            <CardTitle>{item.name}</CardTitle>
+            <CardTitle className={"mt-0 text-base font-normal"}>{item.role}</CardTitle>
+            <div className="mt-4 text-zinc-400 tracking-wide leading-relaxed text-sm" dangerouslySetInnerHTML={{ __html: item.description }}></div>
+            {/* <CardDescription dangerouslySetInnerHTML={{ __html: item.description }}></CardDescription> */}
+          </Card>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export const Card = ({
+  className,
+  children,
+}) => {
+  return (
+    <div
+      className={cn(
+        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        className
+      )}
+    >
+      <div className="relative z-50 ">
+        <div className="p-4 flex flex-col justify-center
+        items-center">{children}</div>
+      </div>
+    </div>
+  );
+};
+export const CardTitle = ({
+  className,
+  children,
+}) => {
+  return (
+    <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
+      {children}
+    </h4>
+  );
+};
+export const CardDescription = ({
+  className,
+  children,
+}) => {
+  return (
+    <p
+      className={cn(
+        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
+        className
+      )}
+    >
+      {children}
+    </p>
+  );
+};

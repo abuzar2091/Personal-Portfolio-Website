212bcb513809a6c9f68c227acec811992c396000
Abuzar <abuzar0561446@gmail.com>
2024-04-30 05:05:55 +0530

    Importing component in main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,26 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
+import { AboutMe} from "@/components/AboutMe";
+import { MySkills } from "@/components/MySkills";
+import Services from "@/components/Services";
+import ContactMe from "@/components/ContactMe";
+import Footer from "@/components/Footer";
+import Project from "@/components/Project";
+import WorkExperience from "@/components/WorkExperience";
 
 
 export default function Home() {
   return (
     <main className=" min-h-screen text-white bg-black/[.9]  antialiased">
-     
       <Header/>
-      <HeroSection/>
+      <HeroSection id="home"/>
+      <AboutMe/>
+      <MySkills/>
+      <Project/>
+      <WorkExperience/>
+      <Services/>
+      <ContactMe/>
+      <Footer/>
     </main>
   );
 }
